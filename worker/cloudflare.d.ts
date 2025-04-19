39f59cd23938d39daf23bfcbba9b1dbc21ae14de
Kevin Damm <github@kevindamm.com>
2025-04-18 22:45:11 -0400

    env.d.ts is split into cloudflare types and worker/env bindings

---
+++ worker/cloudflare.d.ts
@@ -0,0 +1,18 @@
+
+import type {
+  CfProperties,
+  ExecutionContext,
+} from "@cloudflare/workers-types";
+
+declare module "h3" {
+  interface H3EventContext {
+    cf: CfProperties;
+    cloudflare: {
+      request: Request;
+      env: Env;
+      context: ExecutionContext;
+    };
+  }
+}
+
+export {};
