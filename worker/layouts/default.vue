0513fb22cfcf381331e570b1037244319c6de33e
Kevin Damm <github@kevindamm.com>
2025-04-19 21:40:33 -0400

    the most basic of app.vue root component

---
+++ worker/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+      <h1>Mic Memo</h1>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <footer>default layout footer content...</footer>
+</template>
+
+<script setup lang="ts">
+// TODO... signal handler for add-note, update-note, start-record, stop-record
+</script>
+
+<style>
+@layer layout {
+
+}
+</style>
