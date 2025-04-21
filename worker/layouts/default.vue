a19f4e1c9d33b2e6a8ed05e984ae5fedb7923043
Kevin Damm <github@kevindamm.com>
2025-04-20 23:58:48 -0400

    basic layout, and a README for the workers source

--- worker/layouts/default.vue
+++ worker/layouts/default.vue
@@ -1,21 +1,34 @@
 <template>
-    <header>
-      <h1>Mic Memo</h1>
-    </header>
+<h1>Mic Memo</h1> <!-- hide in some layouts? -->
 
-    <main>
-      <slot />
-    </main>
+<main class="container-fluid">
+  <slot />
+</main>
 
-    <footer>default layout footer content...</footer>
+<footer class="container">
+  <div>Copyright &copy; 2025 Kevin Damm, all rights reserved.</div>
+  <!-- TODO link terms of service, source code repo,  -->
+</footer>
 </template>
 
 <script setup lang="ts">
 // TODO... signal handler for add-note, update-note, start-record, stop-record
 </script>
 
-<style>
+<style scoped>
 @layer layout {
+
+h1 {
+  padding-top: .5lh;
+  text-align: center;
+}
+
+footer.container {
+  bottom: 0;
+  text-align: center;
+  padding-bottom: 1lh;
+  margin: auto;
+}
 
 }
 </style>
