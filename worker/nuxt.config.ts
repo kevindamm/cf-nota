141257d95d862c7cc71afc0226399343923d9834
Kevin Damm <github@kevindamm.com>
2025-04-22 08:17:52 -0400

    kinde route setup

--- worker/nuxt.config.ts
+++ worker/nuxt.config.ts
@@ -1,4 +1,4 @@
-import nitroCloudflareBindings from "nitro-cloudflare-dev";
+import nitroCloudflareBindings from 'nitro-cloudflare-dev';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -6,16 +6,31 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   nitro: {
-    preset: "cloudflare_module",
-    // // Pages-like routing behavior
-    // prerender: {
-    //   autoSubfolderIndex: false
-    // },
+    preset: 'cloudflare_module',
+    // Pages-like routing behavior - does not generate /index.html in URL.
+    prerender: {
+      autoSubfolderIndex: false
+    },
+    /* database layer is still experimental
+    database: {
+      connector: 'sqlite',
+      options: { name: 'db' }},
+    // users: { ??? cloudflare D1 & token },
+    */
     cloudflare: {
       deployConfig: true,
       nodeCompat: true
-    }
-  },
+    }},
+
+  kinde: {
+    debug: false,
+    endpoints: {
+      callback: '/kindecall',
+      login: '/auth/login',
+      logout: '/auth/logout',
+      register: '/auth/register',
+      health: '/_health',
+    }},
 
   router: {
     options: {
@@ -23,9 +38,36 @@ export default defineNuxtConfig({
     }
   },
 
+  routeRules: {
+    '/**': {
+      appMiddleware: ['auth-logged-in'],
+      kinde: {
+        redirectUrl: '/auth/login',
+        external: true,
+      }},
+    '/': {
+      appMiddleware: ['auth-logged-in'],
+      kinde: {
+        public: true,
+      }},
+    '/author': {
+      appMiddleware: ['auth-logged-in'],
+      kinde: {
+        permissions: {
+          author: true,
+        },
+        redirectUrl: '/auth/login',
+        external: true,
+      }}
+  },
+
   // One root CSS where dependencies are imported into layers, making cascading explicit.
   css: ['~/assets/css/app.css'],
 
   // zero-config modules are defined here
-  modules: ["nitro-cloudflare-dev", "@vueuse/nuxt", "@nuxtjs/kinde"]
+  modules: [
+    nitroCloudflareBindings,
+    '@vueuse/nuxt',
+    '@nuxtjs/kinde',
+  ]
 })
