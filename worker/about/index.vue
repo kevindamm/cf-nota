3e842104acd63786903edf3e33015f5df4050072
Kevin Damm <github@kevindamm.com>
2025-06-17 22:22:43 -0700

    table of contents for "about" pages

--- worker/about/index.vue
+++ worker/about/index.vue
@@ -1,9 +1,26 @@
-<!--
-
--->
-
 <template>
-  Introduction &amp;
+<div>
+Learn about how this site was made
 
-  Interactive Table of contents...
+<ul>
+  <li>setting up Nuxt.js on Cloudflare Workers</li>
+  <li>defining page layouts and interactivity with components</li>
+  <li>using workers as a cache, database proxy and scalable compute</li>
+  <li>database schema design, and what should be stored there (economically)</li>
+  <li>authentication and authorization</li>
+  <li>key-value mappings using cloudflare KV</li>
+  <li>microphone interface as a Vue component</li>
+  <li>media storage schema design, and storing media in R2</li>
+  <li>durable objects as row transactions that go to sleep</li>
+  <li>sharing media between users (i.e., roles and permissions)</li>
+  <li>moderation (because you know it's only a matter of time...)</li>
+  <li>rate limiting (a soft barrier for abusive behavior)</li>
+  <li>metrics and monitoring (you want observability when things go awry)</li>
+  <li>marketing and SEO, what is important?</li>
+</ul>
+
+TODO: interactive system diagram with call-outs for many of the above sections
+</div>
 </template>
+
+
